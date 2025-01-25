8fa4d5cb7574a57fc9f6cce498e6c1a79dfb241a
Pasha Stierle <pasha.king.stierle@gmail.com>
2025-01-25 12:13:45 -0800

    Removed Fmod

--- Assets/Scripts/PlayerInputManagerTest.cs
+++ Assets/Scripts/PlayerInputManagerTest.cs
@@ -3,16 +3,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputManagerTest : MonoBehaviour
 {
-    private void OnEnable()
-    {
-        //PlayerInputManager.instance.playerJoinedEvent.AddListener(OnPlayerJoined);
-    }
-
-    private void OnDisable()
-    {
-        //PlayerInputManager.instance.playerJoinedEvent.RemoveListener(OnPlayerJoined);
-    }
-
     protected void OnPlayerJoined(PlayerInput playerInput)
     {
         Debug.Log("Player Joined: " + playerInput.playerIndex);
