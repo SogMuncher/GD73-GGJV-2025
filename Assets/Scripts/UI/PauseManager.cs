4beaf55a940b1e398915176509e84106eac85d55
Vish <vishepa@myyahoo.com>
2025-01-25 12:10:00 -0800

    pause menu fixed i think

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -4,17 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-    public UnityEvent OnResume;
+    public UnityEvent OnP1Resume;
+    public UnityEvent OnP2Resume;
  
     public void Pause()
     {
         Time.timeScale = 0f;
     }
 
-    public void Resume()
+    public void ResumeP1()
     {
         Time.timeScale = 1f;
-        OnResume.Invoke();
+        OnP1Resume.Invoke();
+
+    }
+    public void ResumeP2()
+    {
+        Time.timeScale = 1f;
+        OnP2Resume.Invoke();
+
 
     }
 
