c6499dff5f2b2d1eb9c63b6b579dd9a37f46135d
Vish <vishepa@myyahoo.com>
2025-01-26 00:16:35 -0800

    Countdown, more to do yet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.ComponentModel;
 using TMPro;
 using UnityEngine;
@@ -11,17 +12,24 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     // Array to store player scores
     private int[] playerScores;
+    private int[] playerRoundsWon;
 
     // PlayerInputManager reference
     private PlayerInputManager playerInputManager;
     private PlayerInput playerInput;
 
     public TextMeshProUGUI[] playerScoreTexts;
+    public TextMeshProUGUI[] playerRoundsWonTexts;
     public TextMeshProUGUI P1WinText;
     public TextMeshProUGUI P2WinText;
+    public TextMeshProUGUI RoundStartText;
+
+    [SerializeField] private float _roundStartTimer = 1f;
 
     public UnityEvent OnP1Win;
     public UnityEvent OnP2Win;
+
+    public UnityEvent OnRoundEnd;
 
     
     [SerializeField]private float _maxScore = 2;
@@ -34,6 +42,7 @@ public class GameManager : MonoBehaviour
 
         // Initialize player scores array 
         playerScores = new int[playerInputManager.playerCount];
+        playerRoundsWon = new int[playerInputManager.playerCount];
 
         if (playerScoreTexts.Length != playerInputManager.playerCount)
         {
@@ -42,6 +51,10 @@ public class GameManager : MonoBehaviour
         }
 
         
+    }
+    private void Start()
+    {
+        StartCoroutine(RoundStart());
     }
 
     // Function to increment player score based on player index
@@ -54,18 +67,38 @@ public class GameManager : MonoBehaviour
 
             if (playerScores[0] == 2)
             {
-                
-                P1Win("Player 1 wins!");
+
+                IncrementPlayerRoundsWon(playerIndex);
+                playerScores[0] = 0;
+                UpdateUIScore();
+                OnRoundEnd.Invoke();
+                StartCoroutine(RoundStart());
             }
             if (playerScores[1] == 2)
             {
                 
-                P2Win("Player 2 wins!");
+                IncrementPlayerRoundsWon(playerIndex);
+                playerScores[1] = 0;
+                UpdateUIScore();
+                OnRoundEnd.Invoke();
+                StartCoroutine(RoundStart());
+
+
+
             }
 
             Debug.Log(playerScores[0]);
             Debug.Log(playerScores[1]);
 
+        }
+    }
+
+    public void IncrementPlayerRoundsWon(int playerIndex)
+    {
+        if (playerIndex >= 0 && playerIndex < playerScores.Length)
+        {
+            playerRoundsWon[playerIndex]++;
+            UpdateUIScore();
         }
     }
 
@@ -79,12 +112,18 @@ public class GameManager : MonoBehaviour
         return 0;
     }
 
+
     
     public void UpdateUIScore()
     {
         for (int i = 0; i < playerScores.Length; i++)
         {
             playerScoreTexts[i].text = "Player " + (i + 1) + ": " + playerScores[i];
+        }
+
+        for (int i = 0; i < playerRoundsWon.Length; i++)
+        {
+            playerRoundsWonTexts[i].text = "Rounds Won: " + playerRoundsWon[i];
         }
     }
 
@@ -97,5 +136,41 @@ public class GameManager : MonoBehaviour
     {
         OnP2Win.Invoke();
         P2WinText.text = winnerString;
+    }
+    
+    public IEnumerator RoundStart()
+    {
+        Time.timeScale = 0f;
+        RoundStartText.text = "Round Starting in";
+
+        yield return new WaitForSecondsRealtime(_roundStartTimer + 1f);
+
+        RoundStartText.text = "3";
+
+        yield return new WaitForSecondsRealtime(_roundStartTimer);
+
+
+        RoundStartText.text = "2";
+
+        yield return new WaitForSecondsRealtime(_roundStartTimer);
+
+
+
+        RoundStartText.text = "1";
+
+        yield return new WaitForSecondsRealtime(_roundStartTimer);
+
+
+        RoundStartText.text = "Fight";
+
+        Time.timeScale = 1f;
+
+        yield return new WaitForSecondsRealtime(_roundStartTimer);
+
+        RoundStartText.text = "";
+
+
+        yield break;
+
     }
 }
