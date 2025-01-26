24b85cf04e836aef6b0ca563616a1814cd93996b
Vish <vishepa@myyahoo.com>
2025-01-25 21:09:33 -0800

    Fixed PAssages HOLY FUCK GIT

--- Assets/Scripts/Environment/VerticalPassage.cs
+++ Assets/Scripts/Environment/VerticalPassage.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class VerticalPassage : MonoBehaviour
 {
 
+    [SerializeField] private Transform _connector;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        // Get the collision object's initial X position
+        float collisionInitialX = collision.gameObject.transform.position.x;
+        Debug.Log(collisionInitialX);
+        Debug.Log("Collider" + collision.gameObject.name);
+        // Reposition the collision object
+        Vector3 newPosition = new Vector3(collisionInitialX, _connector.position.x, _connector.position.z);
+        newPosition.x = collisionInitialX; // Maintain the collision object's initial X
+        collision.gameObject.transform.position = newPosition;
     }
 }
